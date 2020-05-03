87caa97bb041ee99e28487335eb88cc02db6e06d
nikeshthapa255 <nikeshthapa255@gmail.com>
2020-05-03 17:04:43 +0530

    hebbian and winnow added

--- 5)McCulloh-Pitts.py
+++ 5)McCulloh-Pitts.py
@@ -15,15 +15,29 @@ import matplotlib.pyplot as plt
 from BasicFunctions.NeuralNetwork import *
 
 class McCulloh:
-    def __init__(self, inputFile, outputFile="output_MCP.txt"):
+    def __init__(self, inputFile, theta, outputFile="output_MCP.txt"):
         self.inputFile = inputFile
         self.outputFile = outputFile
         self.X, self.Y = takeInput(self.inputFile)
         print('Parameters entered!')
         self.test_cases = len(self.X)
+        self.num_attributes = len(self.X[0])
+        self.weigths =  setRandomWeights(*[self.num_attributes, 1])
+        self.theta = theta
 
-    def train(self):
+    def get_net(self, x1):
+        x_mini = x1.reshape(1, self.num_attributes)
+        net = np.dot(x_mini, self.weigths)
+        return net
+    # def train(self):
 
-    def test(self):
+    def test(self, x1):
+        x1 = np.array([1] + x1)
+        print('TESt INPut - ', x1)
+        net = np.sum((self.get_net(x1)>self.theta) + 0)
+        return net
+        
+
+
 
 # TODO
--- Winnow.py
+++ Winnow.py
@@ -1,64 +1,86 @@
 """
 Winnow model 
 wi+1 = wi*(alpha^(t-a))
+
+Hebbian 
+wi+1 = wi + t*xi
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 from BasicFunctions.NeuralNetwork import *
 
+def hx_hebbian(net):
+    a =  sum([1 if i else -1 for i in (net>0).flatten()])
+    return a
+
+def hx_willow(net, theta):
+    a =  np.sum((net>theta) + 0)
+    return a
+
+def update_weights_winnow( t, a, weights, Alpha):
+    # print("change weights", t, a)
+    w1 = weights*(Alpha**(t-a + 0.0))
+    return w1
+
+def update_weights_hebbian(t, a, weights, x1):
+    print('HB - ', t, x1)
+    w1 = weights + t*(x1.T)
+    return w1
 class Winnow:
     def __init__(self, inputFile, outputFile="output_MCP.txt"):
         self.inputFile = inputFile
         self.outputFile = outputFile
         self.X, self.Y = takeInput(self.inputFile)
-        print('Parameters entered!')
+        print('Parameters entered!', self.Y)
         self.test_cases = len(self.X)
         self.num_attributes = len(self.X[0])
-        self.weigths =  setRandomWeights(*[self.num_attributes, 1])
+        self.weights =  setRandomWeights(*[self.num_attributes, 1])
         self.Alpha = 2
         self.theta = self.num_attributes - 0.1
+        self.hx = lambda net: hx_hebbian(net)
+        # self.hx = lambda net: hx_willow(net, self.theta)
+        self.update_weights = lambda t, a, x1: update_weights_hebbian(t, a, self.weights, x1)
+        # self.update_weights = lambda t, a, x1: update_weights_winnow(t, a, self.weights, self.Alpha)
         self.train()
-    def get_net(self, x1):
-        x_mini = self.X[i].reshape(1, self.num_attributes)
-        net = np.dot(x_mini, self.weigths)
-        return net
 
-    def hx_willow(self, net):
-        a =  np.sum((net>self.theta) + 0)
-        return a
+    def get_net(self, x_mini):
+        net = np.dot(x_mini, self.weights)
+        return net
     
-    def update_weights_winnow(self, t, a):
-        print("change weights", t, a)
-        w1 = self.weigths*(self.Alpha**(t-a + 0.0))
-        self.weigths = w1
-    
+    def set_x(self, x1):
+        x_mini = x1.reshape(1, self.num_attributes)
+        return x_mini
+
     def train(self, epoch = 10):
         print('Training Begin')
         for _ in range(epoch):
             ch = 0
             for i in range(self.test_cases):
-                print('weights - ', self.weigths.T)
+                print('weights - ', self.weights.T)
                 t = self.Y[i, 0]
-                net = self.get_net(self.X[i])
-                print('x - {}, net = {}, threshold - {}'.format(x_mini, net, self.theta))
+                x1 = self.set_x(self.X[i])
+                net = self.get_net(x1)
+                a = self.hx(net)
+                print('x - {}, net = {}, threshold - {}'.format(x1, net, a))
                 # update weights
-                a = self.hx_willow(net)
                 if t!=a:
                     ch+=1
-                    self.update_weights_winnow(t, a)
+                    self.weights = self.update_weights(t, a, x1)
             if ch==0:
                 break
         print('END of Training for {} epochs'.format(epoch))
 
     def test(self, x1):
-        x1 = np.array(x1)
+        x1 = np.array([1] + x1)
+        print('TESt INPut - ', x1)
         net = self.get_net(x1)
-        a = self.hx_willow(net)
+        a = self.hx(net) # winnow
+
         return a
         
 
 inputFile = "INPUT.txt"
 
-classification_test(Winnow, inputFile)
+classification_test(Winnow, inputFile, type=int)
 
